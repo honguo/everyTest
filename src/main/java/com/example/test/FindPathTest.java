80d739be4fe20bc05ba9057497cbe9644a65a2e1
yp-tc-m-4820 <jiahong.li@yeepay.com>
2019-12-13 18:05:10 +0800

    二叉搜索树后序序列

--- src/main/java/com/example/test/FindPathTest.java
+++ src/main/java/com/example/test/FindPathTest.java
@@ -1,23 +1,48 @@
 package com.example.test;
 
 import java.util.ArrayList;
-import java.util.Stack;
 
 public class FindPathTest {
+    private ArrayList<ArrayList<Integer>> result = new ArrayList<>();
 
-//    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
-//        if(root == null){
-//            return null;
-//        }
-//        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-//        Stack<TreeNode> stack = new Stack<>();
-//        stack.push(root);
-//        while (stack.peek() != null){
-//            TreeNode node = stack.pop();
-//            if(node.left != null){
-//                stack.push(node.left);
-//                result.add()
-//            }
-//        }
-//    }
+    public static void main(String[] args){
+        TreeNode root1 = new TreeNode(8);
+        root1.left = new TreeNode(8);
+        root1.right = new TreeNode(7);
+        root1.left.left = new TreeNode(9);
+        root1.left.right = new TreeNode(2);
+        root1.left.right.left = new TreeNode(4);
+        root1.left.right.right = new TreeNode(7);
+        root1.right.left = new TreeNode(8);
+        root1.right.left.left = new TreeNode(2);
+        FindPathTest findPathTest = new FindPathTest();
+        ArrayList<ArrayList<Integer>> re = findPathTest.FindPath(root1,25);
+        System.out.println(re);
+    }
+
+
+
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+        ArrayList<Integer> n = new ArrayList<>();
+        FindPath(root,target,n);
+        return result;
+    }
+
+    public void FindPath(TreeNode root, int target,ArrayList<Integer> list){
+        if(root.left == null && root.right == null){
+            if(root.val == target){
+                list.add(root.val);
+                result.add(list);
+            }
+        }else {
+            list.add(root.val);
+            if(root.left != null){
+                FindPath(root.left,target-root.val,list);
+            }
+            if(root.right != null){
+                FindPath(root.right,target-root.val,list);
+            }
+        }
+    }
+
 }
